0bd5ef48a5c0ef620bfc500c8a453813216a2745
Олег Александров <olegaleksandrov@MacBook-Pro-Oleg.local>
2020-10-04 20:45:54 +0300

    update

--- src/main/java/me/olegthelilfix/mykata/four/BlockSequence.kt
+++ src/main/java/me/olegthelilfix/mykata/four/BlockSequence.kt
@@ -1,5 +1,13 @@
 package me.olegthelilfix.mykata.four
 
+/**
+ * Consider the following array:
+ * [1, 12, 123, 1234, 12345, 123456, 1234567, 12345678, 123456789, 12345678910, 1234567891011...]
+ * If we join these blocks of numbers, we come up with an infinite sequence which starts with 112123123412345123456....The list is infinite.
+ * You will be given an number (n) and your task will be to return the element at that index in the sequence, where 1 ≤ n ≤ 10^18.
+ * Assume the indexes start with 1, not 0.
+ * origin https://www.codewars.com/kata/5e1ab1b9fe268c0033680e5f/train/kotlin
+ */
 object BlockSequence {
     fun solve(n: Long): Int {
         // your code goes here
